e87bc71e08de3584209f8d9fe9d90192d207e671
Glazelf <gideon.steenbergen@gmail.com>
2020-11-16 12:28:54 +0100

    Some formatting and comments

--- Extract.js
+++ Extract.js
@@ -26,18 +26,20 @@ var displayHtml = function () {
             for (let index2 = 0; index2 < pokemon.length; index2++) {
                 pokemonText = pokemon[index2].innerText;
 
-                const nl1 = pokemonText.indexOf('\n'); //after this: ability
-                const nl2 = pokemonText.indexOf('\n', nl1 + 1); //after this: level
-                const nl3 = pokemonText.indexOf('\n', nl2 + 1); //after this: EVs
-                const nl4 = pokemonText.indexOf('\n', nl3 + 1); //after this: Nature
-                const nl5 = pokemonText.indexOf('\n', nl4 + 1); //after this: IVs
-                const nl6 = pokemonText.indexOf('\n', nl5 + 1); //after this: Move 1
-                const nl7 = pokemonText.indexOf('\n', nl6 + 1); //after this: Move 2
-                const nl8 = pokemonText.indexOf('\n', nl7 + 1); //after this: Move 3
-                const nl9 = pokemonText.indexOf('\n', nl8 + 1); //after this: Move 4
+                const nl1 = pokemonText.indexOf('\n'); // After this: ability
+                const nl2 = pokemonText.indexOf('\n', nl1 + 1); // After this: level
+                const nl3 = pokemonText.indexOf('\n', nl2 + 1); // After this: EVs
+                const nl4 = pokemonText.indexOf('\n', nl3 + 1); // After this: Nature
+                const nl5 = pokemonText.indexOf('\n', nl4 + 1); // After this: IVs
+                const nl6 = pokemonText.indexOf('\n', nl5 + 1); // After this: Move 1
+                const nl7 = pokemonText.indexOf('\n', nl6 + 1); // After this: Move 2
+                const nl8 = pokemonText.indexOf('\n', nl7 + 1); // After this: Move 3
+                const nl9 = pokemonText.indexOf('\n', nl8 + 1); // After this: Move 4
 
+                // Seperate name from item
                 const endName = pokemonText.search('@') - 1;
                 let name = pokemonText.slice(0, endName);
+                // No more gender
                 name = name.replace(' (M)', '');
                 name = name.replace(' (F)', '');
 
@@ -53,6 +55,8 @@ var displayHtml = function () {
                 let move2;
                 let move3;
                 let move4;
+
+                // Check for non-31 IVs then seperate moves accordingly
                 if (pokemonText.charAt(nl5 + 1) == "I") {
                     move1 = pokemonText.slice(nl6 + 3, nl7 - 2);
                     move2 = pokemonText.slice(nl7 + 3, nl8 - 2);
@@ -66,6 +70,7 @@ var displayHtml = function () {
                 };
                 arrayData[index2 + (index * 6)] = [name, item, ability, nature, move1, move2, move3, move4];
             };
+
             if (index == serverPath.length - 1) {
                 export_csv(arrayHeader, arrayData, ',', 'Teams');
             };
@@ -74,7 +79,8 @@ var displayHtml = function () {
     return false;
 };
 
-const arrayHeader = ["Pokemon", "item", "ability", "nature", "move1", "move2", "move3", "move4"]
+const arrayHeader = ["Pokemon", "item", "ability", "nature", "move1", "move2", "move3", "move4"];
+
 const export_csv = (arrayHeader, arrayData, delimiter, fileName) => {
     let header = arrayHeader.join(delimiter) + '\n';
     let csv = header;
@@ -91,4 +97,3 @@ const export_csv = (arrayHeader, arrayData, delimiter, fileName) => {
     hiddenElement.download = fileName + '.csv';
     hiddenElement.click();
 };
-
